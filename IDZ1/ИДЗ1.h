f52e4e75d4928d16c9253d6bdb71102289fc9345
UGRINOVICH CODE <91239358+pixxbe@users.noreply.github.com>
2024-09-22 18:25:18 +1000

    Создал базовые функции

---
+++ IDZ1/ИДЗ1.h
@@ -0,0 +1,77 @@
+#ifndef ИДЗ1_hpp
+#define ИДЗ1_hpp
+
+#include <iostream>
+#include "ИДЗ1.h"
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+// Добавление элемента в начало (Инициализация)
+void PushUp(Node*& head, int value)
+{
+    Node* new_node = new Node;
+    new_node->data = value;
+    new_node->next = head;
+    head = new_node;
+}
+
+// Печать всего списка
+void PrintList(Node* head)
+{   
+    if (head == nullptr)
+    {
+        cout << "Список пуст" << endl;
+    }
+    else
+    {
+        Node* temp = head;
+        cout << "NULL -> ";
+        while (temp != nullptr)
+        {
+            cout << "адрес: [" << temp->next << "] значение: " << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+
+}
+
+// Добавление в конец списка 
+void PushBack(Node*& head, int value)
+{
+    Node* new_node = new Node;
+    new_node->data = value;
+    new_node->next = nullptr;
+    if (head == nullptr)
+    {
+        head = new_node;
+    }
+    else
+    {
+        Node* temp = head;
+        while (temp->next != nullptr)
+        {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+}
+
+// Освобождение памяти (удалние всех элементов спсика)
+void DeleteAllList(Node*& head)
+{   
+    while (head != nullptr)
+    {
+        Node* temp = head;
+        head = temp->next; //Так же тут можно написать и head->next; т.к темп указывает на head  содержит его данные
+        delete temp;
+    }
+    
+}
+
+#endif /* ИДЗ1_hpp */#pragma once
--- IDZ1/ИДЗ1_ФСиД.cpp
+++ IDZ1/ИДЗ1_ФСиД.cpp
@@ -1,1 +1,15 @@
-.
+﻿// ИДЗ1_ФСиД.cpp
+#include "ИДЗ1.h"
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    Node* List = nullptr;
+    PushUp(List, 3);
+    PushUp(List, 15);
+    PushBack(List, 2);
+    PrintList(List);
+    DeleteAllList(List);
+    PrintList(List);
+}
+
